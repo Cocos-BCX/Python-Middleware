d3e0b72559b4953a157461d1e69a0e10d0f857e1
gkany <43233929+gkany@users.noreply.github.com>
2019-07-31 17:28:37 +0800

    update requests version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     ],
     install_requires=[
         "ecdsa==0.13",
-        "requests==2.19.1",
+        "requests==2.22.0",
         "websocket-client==0.48.0",
         "pylibscrypt==1.7.1",
         "pycryptodome==3.6.6",
