25fa508abf52e9a29d4839d2674da4e2900e27a2
gkany <43233929+gkany@users.noreply.github.com>
2019-11-12 17:41:07 +0800

    update setup

--- setup.py
+++ setup.py
@@ -48,8 +48,8 @@ setup(
         'Topic :: Office/Business :: Financial',
     ],
     install_requires=[
-        "ecdsa==0.13",
-        "requests==2.19.1",
+        "ecdsa==0.13.3",
+        "requests==2.20.0",
         "websocket-client==0.48.0",
         "pylibscrypt==1.7.1",
         "pycryptodome==3.6.6",
