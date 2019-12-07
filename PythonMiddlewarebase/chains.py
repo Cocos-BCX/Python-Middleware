31939aee68b156f5cd6ff5b9c1a4ea8bb32c5601
xukang <xukang@chukong-inc.com>
2019-12-07 13:35:57 +0800

    add known chains

--- PythonMiddlewarebase/chains.py
+++ PythonMiddlewarebase/chains.py
@@ -1,8 +1,19 @@
 default_prefix = "COCOS"
 
 known_chains = {
-    "COCOS": {
-        "chain_id": "c1ac4bb7bd7d94874a1cb98b39a8a582421d03d022dfa4be8c70567076e03ad0",
+    "prod": {
+        "chain_id": "6057d856c398875cac2650fe33caef3d5f6b403d184c5154abbff526ec1143c4",
         "core_symbol": "COCOS",
-        "prefix": "COCOS"}
+        "prefix": "COCOS"
+    },
+    "testnet": {
+        "chain_id": "1ae3653a3105800f5722c5bda2b55530d0e9e8654314e2f3dc6d2b010da641c5",
+        "core_symbol": "COCOS",
+        "prefix": "COCOS"
+    },
+    "local": {
+        "chain_id": "179db3c6a2e08d610f718f05e9cc2aabad62aff80305b9621b162b8b6f2fd79f",
+        "core_symbol": "COCOS",
+        "prefix": "COCOS"
     }
+}
